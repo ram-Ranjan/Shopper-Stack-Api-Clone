5d093f6d0fdcecb5564b83222ccb7bd53ba5042f
Sharathc18 <122531284+Sharathc18@users.noreply.github.com>
2023-05-17 18:45:56 +0530

    customercart and customerorder entity added

---
+++ src/main/java/com/ramRanjan/ShopperStackApiClone/entity/CustomerCart.java
@@ -0,0 +1,17 @@
+package com.ramRanjan.ShopperStackApiClone.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CustomerCart {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long customerCartId;
+	private double customerCartSubTotal;
+	private long customerCartQuantity;
+	private double customerCartDiscount;
+	private double customerCartTotal;
+}
---
+++ src/main/java/com/ramRanjan/ShopperStackApiClone/entity/CustomerOrder.java
@@ -0,0 +1,17 @@
+package com.ramRanjan.ShopperStackApiClone.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CustomerOrder {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private long customerOrderId;
+private LocalDateTime customerOrderDate;
+private String customerOrderStatus;
+}

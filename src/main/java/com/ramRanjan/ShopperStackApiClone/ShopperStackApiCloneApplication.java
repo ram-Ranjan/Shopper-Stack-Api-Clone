b7f49127b282342e19a3b4ec6f38fd4ee09142a0
ram-Ranjan <ranjanirl3107@gmail.com>
2023-05-17 17:11:36 +0530

    added starter files

---
+++ src/main/java/com/ramRanjan/ShopperStackApiClone/ShopperStackApiCloneApplication.java
@@ -0,0 +1,13 @@
+package com.ramRanjan.ShopperStackApiClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopperStackApiCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopperStackApiCloneApplication.class, args);
+	}
+
+}

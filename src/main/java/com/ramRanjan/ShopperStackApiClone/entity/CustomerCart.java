6b080b6452a8a50bdab127ff0014f2df6295e4bf
unknown <poojithadarshan2131@gmail.com>
2023-05-18 11:12:07 +0530

    committing no changes

--- src/main/java/com/ramRanjan/ShopperStackApiClone/entity/CustomerCart.java
+++ src/main/java/com/ramRanjan/ShopperStackApiClone/entity/CustomerCart.java
@@ -5,6 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 public class CustomerCart {
 @Id

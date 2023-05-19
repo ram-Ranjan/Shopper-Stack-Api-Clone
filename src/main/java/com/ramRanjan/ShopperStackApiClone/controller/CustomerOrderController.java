b18606d7146b1bbe61395e44489181d2ebf92e6e
unknown <poojithadarshan2131@gmail.com>
2023-05-19 11:27:21 +0530

    Created Service layer , controller layaer and required exceptions for CustomerOrders

---
+++ src/main/java/com/ramRanjan/ShopperStackApiClone/controller/CustomerOrderController.java
@@ -0,0 +1,58 @@
+package com.ramRanjan.ShopperStackApiClone.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.ramRanjan.ShopperStackApiClone.dto.CustomerOrderDto;
+import com.ramRanjan.ShopperStackApiClone.entity.CustomerOrder;
+import com.ramRanjan.ShopperStackApiClone.service.CustomerOrderService;
+import com.ramRanjan.ShopperStackApiClone.util.ResponseStructure;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+@RequestMapping("/order")
+public class CustomerOrderController {
+	@Autowired
+	private CustomerOrderService orderService;
+	
+	@ApiOperation(value = "Save CustomerOrder", notes = " Api is used to save the CustomerOrder")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Successfully created"),
+			@ApiResponse(code = 404, message = "CustomerOrder not found for the given  id") })
+	@PostMapping
+public ResponseEntity<ResponseStructure<CustomerOrder>> addCustomerOrder(@RequestParam long cartId,@RequestBody CustomerOrderDto orderDto){
+		return orderService.addOrder(cartId, orderDto);
+	}
+	@ApiOperation(value = "Update CustomerOrder", notes = " Api is used to update the CustomerOrder")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Successfully Updated"),
+			@ApiResponse(code = 404, message = "CustomerOrder not found for the given  id") })
+	@PutMapping
+	public ResponseEntity<ResponseStructure<CustomerOrder>> updateCustomerOrder(@RequestParam long orderId,@RequestBody CustomerOrderDto orderDto){
+		return orderService.updateOrder(orderId, orderDto);
+	}
+	@ApiOperation(value = "Find CustomerOrder", notes = " Api is used to find the CustomerOrder")
+	@ApiResponses(value = { @ApiResponse(code = 302, message = "Successfully fetched"),
+			@ApiResponse(code = 404, message = "CustomerOrder not found for the given  id") })
+	@GetMapping
+	public ResponseEntity<ResponseStructure<CustomerOrder>> getCustomerOrder(@RequestParam long orderId){
+		return orderService.getOrder(orderId);
+	}
+
+	@ApiOperation(value = "Delete CustomerOrder", notes = " Api is used to delete the CustomerOrder")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully deleted"),
+			@ApiResponse(code = 404, message = "CustomerOrder not found for the given  id") })
+	@DeleteMapping
+	public ResponseEntity<ResponseStructure<CustomerOrder>> deleteCustomerOrder(@RequestParam long orderId){
+		return orderService.deleteOrder(orderId);
+	}
+
+}
---
+++ src/main/java/com/ramRanjan/ShopperStackApiClone/service/CustomerOrderService.java
@@ -0,0 +1,87 @@
+package com.ramRanjan.ShopperStackApiClone.service;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.ramRanjan.ShopperStackApiClone.dao.CustomerCartDao;
+import com.ramRanjan.ShopperStackApiClone.dao.CustomerOrderDao;
+import com.ramRanjan.ShopperStackApiClone.dto.CustomerOrderDto;
+import com.ramRanjan.ShopperStackApiClone.entity.CustomerCart;
+import com.ramRanjan.ShopperStackApiClone.entity.CustomerOrder;
+import com.ramRanjan.ShopperStackApiClone.exception.CartIdNotFoundException;
+import com.ramRanjan.ShopperStackApiClone.exception.OrderIdNotFoundException;
+import com.ramRanjan.ShopperStackApiClone.util.ResponseStructure;
+
+@Service
+public class CustomerOrderService {
+	@Autowired
+	private CustomerOrderDao orderDao;
+	@Autowired
+	private CustomerCartDao cartDao;
+	@Autowired
+	private ModelMapper modelMapper;
+
+	public ResponseEntity<ResponseStructure<CustomerOrder>> addOrder(long cartId, CustomerOrderDto customerDto) {
+		CustomerCart cart = cartDao.findCart(cartId);
+		if (cart != null) {
+			CustomerOrder order = this.modelMapper.map(customerDto, CustomerOrder.class);
+			order.setCart(cart);
+			CustomerOrder dbCartOrder = orderDao.saveCustomerOrder(order);
+			if (dbCartOrder != null) {
+				ResponseStructure<CustomerOrder> structure = new ResponseStructure<CustomerOrder>();
+				structure.setMessage("Ordered  successfully");
+				structure.setStatus(HttpStatus.CREATED.value());
+				structure.setData(dbCartOrder);
+				return new ResponseEntity<ResponseStructure<CustomerOrder>>(structure, HttpStatus.CREATED);
+			}
+			return null;
+		} else {
+		throw new CartIdNotFoundException("failed to order!");
+
+		}
+	}
+
+	public ResponseEntity<ResponseStructure<CustomerOrder>> updateOrder(long orderId, CustomerOrderDto orderDto) {
+		CustomerOrder order = this.modelMapper.map(orderDto, CustomerOrder.class);
+		CustomerOrder dbOrder =orderDao.updateCustomerOrder(orderId, order);
+		if (dbOrder != null) {
+			ResponseStructure<CustomerOrder> structure = new ResponseStructure<CustomerOrder>();
+			structure.setMessage("Order updated successfully");
+			structure.setStatus(HttpStatus.OK.value());
+			structure.setData(dbOrder);
+			return new ResponseEntity<ResponseStructure<CustomerOrder>>(structure, HttpStatus.OK);
+		} else {
+			throw new OrderIdNotFoundException("Failed to update Order");
+		}
+	}
+
+	public ResponseEntity<ResponseStructure<CustomerOrder>> getOrder(long orderId) {
+		CustomerOrder order=orderDao.getCustomerOrderById(orderId);
+		if (order != null) {
+			ResponseStructure<CustomerOrder> structure = new ResponseStructure<CustomerOrder>();
+			structure.setMessage("Order Fetched successfully");
+			structure.setStatus(HttpStatus.FOUND.value());
+			structure.setData(order);
+			return new ResponseEntity<ResponseStructure<CustomerOrder>>(structure, HttpStatus.FOUND);
+		} else {
+			throw new CartIdNotFoundException("Failed to fetch CustomerOrder");
+		}
+	}
+
+	public ResponseEntity<ResponseStructure<CustomerOrder>> deleteOrder(long orderId) {
+		CustomerOrder order=orderDao.deleteCustomerOrder(orderId);
+		if (order != null) {
+			ResponseStructure<CustomerOrder> structure = new ResponseStructure<CustomerOrder>();
+			structure.setMessage("CustomerOrder Deleted successfully");
+			structure.setStatus(HttpStatus.OK.value());
+			structure.setData(order);
+			return new ResponseEntity<ResponseStructure<CustomerOrder>>(structure, HttpStatus.OK);
+		} else {
+			throw new CartIdNotFoundException("Failed to delete CustomerOrder");
+		}
+	}
+}

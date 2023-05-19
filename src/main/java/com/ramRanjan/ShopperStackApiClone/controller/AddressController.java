f9282dc6fd9e62ad05e042b8cf0bd9681c87c177
unknown <poojithadarshan2131@gmail.com>
2023-05-19 10:35:04 +0530

    Added Swagger i.e.,,Api responses for Controllers of Address, Customer and User

--- src/main/java/com/ramRanjan/ShopperStackApiClone/controller/AddressController.java
+++ src/main/java/com/ramRanjan/ShopperStackApiClone/controller/AddressController.java
@@ -16,6 +16,10 @@ import com.ramRanjan.ShopperStackApiClone.entity.Address;
 import com.ramRanjan.ShopperStackApiClone.service.AddressService;
 import com.ramRanjan.ShopperStackApiClone.util.ResponseStructure;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/address")
 public class AddressController {
@@ -23,18 +27,32 @@ public class AddressController {
 	@Autowired
 	private AddressService addressService;
 	
+	@ApiOperation(value = "Save Address", notes = " Api is used to save the address")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Successfully created"),
+			@ApiResponse(code = 404, message = "Address not found for the given  id") })
 	@PostMapping
 public ResponseEntity<ResponseStructure<Address>> addAddress(@RequestParam long userId,@RequestBody AddressDto addressDto){
 		return addressService.saveAddress(userId, addressDto);
 	}
+	@ApiOperation(value = "Update Address", notes = " Api is used to update the address")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Successfully Updated"),
+			@ApiResponse(code = 404, message = "Address not found for the given  id") })
 	@PutMapping
 	public ResponseEntity<ResponseStructure<Address>> updateAddress(@RequestParam long addressId,@RequestBody AddressDto addressDto){
 		return addressService.updateAddress(addressId, addressDto);
 	}
+
+	@ApiOperation(value = "Find Address", notes = " Api is used to find the address")
+	@ApiResponses(value = { @ApiResponse(code = 302, message = "Successfully fetched"),
+			@ApiResponse(code = 404, message = "Address not found for the given  id") })
 	@GetMapping
 	public ResponseEntity<ResponseStructure<Address>> getAddress(@RequestParam long addressId){
 		return addressService.findAddress(addressId);
 	}
+
+	@ApiOperation(value = "Delete Address", notes = " Api is used to delete the address")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully deleted"),
+			@ApiResponse(code = 404, message = "Address not found for the given  id") })
 	@DeleteMapping
 	public ResponseEntity<ResponseStructure<Address>> deleteAddress(@RequestParam long addressId){
 		return addressService.deleteAddress(addressId);
--- src/main/java/com/ramRanjan/ShopperStackApiClone/controller/UserController.java
+++ src/main/java/com/ramRanjan/ShopperStackApiClone/controller/UserController.java
@@ -17,23 +17,41 @@ import com.ramRanjan.ShopperStackApiClone.entity.User;
 import com.ramRanjan.ShopperStackApiClone.service.UserService;
 import com.ramRanjan.ShopperStackApiClone.util.ResponseStructure;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
 @Autowired
 private UserService userService;
+
+@ApiOperation(value = "Save User", notes = " Api is used to save the User")
+@ApiResponses(value = { @ApiResponse(code = 201, message = "Successfully created"),
+		@ApiResponse(code = 404, message = "User not found for the given  id") })
 @PostMapping
 public ResponseEntity<ResponseStructure<UserDto>> addUser(@RequestBody User user){
 	return userService.addUser(user);
 }
+@ApiOperation(value = "Update User", notes = " Api is used to update the User")
+@ApiResponses(value = { @ApiResponse(code = 201, message = "Successfully Updated"),
+		@ApiResponse(code = 404, message = "User not found for the given  id") })
 @PutMapping
 public ResponseEntity<ResponseStructure<UserDto>> updateUser(@RequestParam long userId ,@RequestBody User user){
 	return userService.updateUser(userId, user);
 }
+@ApiOperation(value = "Find User", notes = " Api is used to find the User")
+@ApiResponses(value = { @ApiResponse(code = 302, message = "Successfully fetched"),
+		@ApiResponse(code = 404, message = "User not found for the given  id") })
 @GetMapping
 public ResponseEntity<ResponseStructure<UserDto>> findUser(@RequestParam long userId){
 	return userService.findUserById(userId);
 }
+
+@ApiOperation(value = "Delete User", notes = " Api is used to delete the User")
+@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully deleted"),
+		@ApiResponse(code = 404, message = "User not found for the given  id") })
 @DeleteMapping
 public ResponseEntity<ResponseStructure<UserDto>> deleteUser(@RequestParam long userId){
 	return userService.deleteUserById(userId);
